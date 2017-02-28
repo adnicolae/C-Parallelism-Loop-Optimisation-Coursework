ed428d880ef30d7acfb86cf69f9e388dd8c4d9d7
Andrei Nicolae <ad.nicolae@outlook.com>
2017-02-28 12:29:49 +0000

    Proper indentation

--- coursework.c
+++ coursework.c
@@ -29,43 +29,44 @@ void compute() {
     // Loop 1.
 	t0 = wtime();
 	int unroll_n = (N/4) * 4;
-	for (int j = 0; j < N; j++) {
-	    for (int i = 0; i < N; i+=4) {
-	        __m128 xj_v = _mm_set1_ps(x[j]);
-	        __m128 xi_v = _mm_load_ps(&x[i]);
-	        __m128 rx_v = _mm_sub_ps(xj_v, xi_v);
 
-	        __m128 yj_v = _mm_set1_ps(y[j]);
-	        __m128 yi_v = _mm_load_ps(&y[i]);
-	        __m128 ry_v = _mm_sub_ps(yj_v, yi_v);
+	for (int j = 0; j < N; j++) {
+		for (int i = 0; i < N; i+=4) {
+			__m128 xj_v = _mm_set1_ps(x[j]);
+			__m128 xi_v = _mm_load_ps(&x[i]);
+			__m128 rx_v = _mm_sub_ps(xj_v, xi_v);
+
+			__m128 yj_v = _mm_set1_ps(y[j]);
+			__m128 yi_v = _mm_load_ps(&y[i]);
+			__m128 ry_v = _mm_sub_ps(yj_v, yi_v);
+
 			__m128 zj_v = _mm_set1_ps(z[j]);
 			__m128 zi_v = _mm_load_ps(&z[i]);
-	        __m128 rz_v = _mm_sub_ps(zj_v, zi_v);
+			__m128 rz_v = _mm_sub_ps(zj_v, zi_v);
+
 			__m128 r2_v = _mm_mul_ps(rx_v, rx_v) + _mm_mul_ps(ry_v, ry_v) + _mm_mul_ps(rz_v, rz_v) + _mm_set1_ps(eps);
+			__m128 r2inv_v = _mm_div_ps(_mm_set1_ps(1.0f),_mm_sqrt_ps(r2_v));
+			__m128 r6inv_1v = _mm_mul_ps(r2inv_v, r2inv_v);
+			__m128 r6inv_v = _mm_mul_ps(r6inv_1v, r2inv_v);
 
-	    __m128 r2inv_v = _mm_div_ps(_mm_set1_ps(1.0f),_mm_sqrt_ps(r2_v));
+			__m128 mj_v = _mm_set1_ps(m[j]);
+			__m128 s_v = _mm_mul_ps(mj_v, r6inv_v);
 
-	    __m128 r6inv_1v = _mm_mul_ps(r2inv_v, r2inv_v);
-	    __m128 r6inv_v = _mm_mul_ps(r6inv_1v, r2inv_v);
+			__m128 axi_v = _mm_load_ps(&ax[i]);
+			__m128 ayi_v = _mm_load_ps(&ay[i]);
+			__m128 azi_v = _mm_load_ps(&az[i]);
 
-	    __m128 mj_v = _mm_set1_ps(m[j]);
-	    __m128 s_v = _mm_mul_ps(mj_v, r6inv_v);
+			__m128 srx_v = _mm_mul_ps(s_v, rx_v);
+			__m128 sry_v = _mm_mul_ps(s_v, ry_v);
+			__m128 srz_v = _mm_mul_ps(s_v, rz_v);
 
-	    __m128 axi_v = _mm_load_ps(&ax[i]);
-	    __m128 ayi_v = _mm_load_ps(&ay[i]);
-	    __m128 azi_v = _mm_load_ps(&az[i]);
+			axi_v = _mm_add_ps(axi_v, srx_v);
+			ayi_v = _mm_add_ps(ayi_v, sry_v);
+			azi_v = _mm_add_ps(azi_v, srz_v);
 
-	    __m128 srx_v = _mm_mul_ps(s_v, rx_v);
-	    __m128 sry_v = _mm_mul_ps(s_v, ry_v);
-	    __m128 srz_v = _mm_mul_ps(s_v, rz_v);
-
-		axi_v = _mm_add_ps(axi_v, srx_v);
-		ayi_v = _mm_add_ps(ayi_v, sry_v);
-		azi_v = _mm_add_ps(azi_v, srz_v);
-
-		_mm_store_ps(&ax[i], axi_v);
-		_mm_store_ps(&ay[i], ayi_v);
-		_mm_store_ps(&az[i], azi_v);
+			_mm_store_ps(&ax[i], axi_v);
+			_mm_store_ps(&ay[i], ayi_v);
+			_mm_store_ps(&az[i], azi_v);
 	    }
 	}
 	t1 = wtime();
