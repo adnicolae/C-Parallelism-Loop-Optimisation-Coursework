8afe404bbd102ec14027ad634d11c20753e78396
Andrew <a.nicolae@warwick.ac.uk>
2017-03-11 20:19:00 +0000

    Attempt at getting rid of a branch and vectorising the if

--- coursework.c
+++ coursework.c
@@ -5,6 +5,8 @@
  * and should not be optimised out. :)
  */
  #include <immintrin.h>
+ #include <stdbool.h>
+
 void compute() {
 
 	double t0, t1;
@@ -48,24 +50,25 @@ void compute() {
         __m128 yj_v = _mm_set1_ps(y[j]);
         __m128 zj_v = _mm_set1_ps(z[j]);
         __m128 mj_v = _mm_set1_ps(m[j]);
-        for (i = 0; i < unroll_n; i+=4) {
 
-			__m128 xi_v = _mm_load_ps(&x[i]);
-			__m128 rx_v = _mm_sub_ps(xj_v, xi_v);
-
-
-			__m128 yi_v = _mm_load_ps(&y[i]);
-			__m128 ry_v = _mm_sub_ps(yj_v, yi_v);
+      for (i = 0; i < unroll_n; i+=4) {
+		    __m128 xi_v = _mm_load_ps(&x[i]);
+		    __m128 rx_v = _mm_sub_ps(xj_v, xi_v);
+		    __m128 yi_v = _mm_load_ps(&y[i]);
+			  __m128 ry_v = _mm_sub_ps(yj_v, yi_v);
 
 
-			__m128 zi_v = _mm_load_ps(&z[i]);
-			__m128 rz_v = _mm_sub_ps(zj_v, zi_v);
+			  __m128 zi_v = _mm_load_ps(&z[i]);
+			  __m128 rz_v = _mm_sub_ps(zj_v, zi_v);
 
 
 
+      // __m128 r2x_v = _mm_add_ps(_mm_mul_ps(rx_v, rx_v), _mm_mul_ps(ry_v, ry_v));
+      // __m128 r2y_v = _mm_add_ps(r2x_v, _mm_mul_ps(rz_v, rz_v));
+			// __m128 r2_v = _mm_add_ps(r2y_v, _mm_set1_ps(eps));
 
-			// __m128 r2_v = _mm_mul_ps(rx_v, rx_v) + _mm_mul_ps(ry_v, ry_v) + _mm_mul_ps(rz_v, rz_v) + _mm_set1_ps(eps);
-			__m128 r2_v = _mm_set1_ps(eps) + rx_v*rx_v + ry_v*ry_v + rz_v*rz_v;   // GNU extension
+			__m128 r2_v = _mm_set1_ps(eps) + rx_v*rx_v + ry_v*ry_v + rz_v*rz_v;
+
 			__m128 r2inv_v = _mm_rsqrt_ps(r2_v);
             // _mm_div_ps(_mm_set1_ps(1.0f),_mm_sqrt_ps(r2_v));
 			__m128 r6inv_1v = _mm_mul_ps(r2inv_v, r2inv_v);
@@ -89,7 +92,8 @@ void compute() {
 			_mm_store_ps(&ay[i], ayi_v);
 			_mm_store_ps(&az[i], azi_v);
 	    }
-        for (; i < N; i++) {
+
+    for (; i < N; i++) {
 			float rx = x[j] - x[i];
 			float ry = y[j] - y[i];
 			float rz = z[j] - z[i];
@@ -138,20 +142,90 @@ void compute() {
     vy[i] += dmp * (dt * ay[i]);
     vz[i] += dmp * (dt * az[i]);
   }
-  
+
 	t1 = wtime();
 	l2 += (t1 - t0);
 
 	// Loop 3.
 	t0 = wtime();
-	for (int i = 0; i < N; i++) {
-		x[i] += dt * vx[i];
-		y[i] += dt * vy[i];
-		z[i] += dt * vz[i];
-		if (x[i] >= 1.0f || x[i] <= -1.0f) vx[i] *= -1.0f;
-		if (y[i] >= 1.0f || y[i] <= -1.0f) vy[i] *= -1.0f;
-		if (z[i] >= 1.0f || z[i] <= -1.0f) vz[i] *= -1.0f;
+  __m128 minus_one = _mm_set1_ps(-1.0f);
+  __m128 one = _mm_set1_ps(1.0f);
+  for (i = 0; i < unroll_n; i+=4) {
+    __m128 x_v = _mm_load_ps(&x[i]);
+    __m128 vx_v = _mm_load_ps(&vx[i]);
+    __m128 dt_v = _mm_set1_ps(dt);
+    __m128 dt_vx_v = _mm_mul_ps(dt_v, vx_v);
 
+    _mm_store_ps(&x[i], _mm_add_ps(x_v, dt_vx_v));
+
+	  // if (x[i] >= 1.0f || x[i] <= -1.0f) vx[i] *= -1.0f;
+    // if ( ((unsigned) (x[i] - (-1.0f)) <= (1.0f - (-1.0f))) == false) vx[i] *= -1.0f;
+    __m128 x_minus_minus_one = _mm_sub_ps(x_v, minus_one);
+    __m128 one_minus_minus_one = _mm_sub_ps(one, minus_one);
+    __m128 mask = _mm_cmple_ps(x_minus_minus_one, one_minus_minus_one);
+
+    __m128 multiply = _mm_mul_ps(vx_v, minus_one);
+    __m128 res = _mm_or_ps(_mm_and_ps(mask, multiply), _mm_andnot_ps(mask, x_minus_minus_one));
+    _mm_store_ps(&vx[i], res);
+
+	}
+
+	for (; i < N; i++) {
+		x[i] += dt * vx[i];
+    // if (x[i] >= 1.0f || x[i] <= -1.0f) vx[i] *= -1.0f;
+        if ( ((unsigned) (x[i] - (-1.0f)) <= (1.0f - (-1.0f))) == false) vx[i] *= -1.0f;
+	}
+
+	for (i = 0; i < unroll_n; i+=4) {
+    __m128 y_v = _mm_load_ps(&y[i]);
+    __m128 vy_v = _mm_load_ps(&vy[i]);
+    __m128 dt_v = _mm_set1_ps(dt);
+    __m128 dt_vy_v = _mm_mul_ps(dt_v, vy_v);
+
+    _mm_store_ps(&y[i], _mm_add_ps(y_v, dt_vy_v));
+
+	  // if (y[i] >= 1.0f || y[i] <= -1.0f) vy[i] *= -1.0f;
+        // if ( ((unsigned) (y[i] - (-1.0f)) <= (1.0f - (-1.0f))) == false) vy[i] *= -1.0f;
+
+        __m128 y_minus_minus_one = _mm_sub_ps(y_v, minus_one);
+        __m128 one_minus_minus_one = _mm_sub_ps(one, minus_one);
+        __m128 mask = _mm_cmple_ps(y_minus_minus_one, one_minus_minus_one);
+
+        __m128 multiply = _mm_mul_ps(vy_v, minus_one);
+        __m128 res = _mm_or_ps(_mm_and_ps(mask, multiply), _mm_andnot_ps(mask, y_minus_minus_one));
+        _mm_store_ps(&vy[i], res);
+	}
+
+	for (; i < N; i++) {
+		y[i] += dt * vy[i];
+	  // if (y[i] >= 1.0f || y[i] <= -1.0f) vy[i] *= -1.0f;
+            if ( ((unsigned) (y[i] - (-1.0f)) <= (1.0f - (-1.0f))) == false) vy[i] *= -1.0f;
+	}
+
+	for (i = 0; i < unroll_n; i+=4) {
+    __m128 z_v = _mm_load_ps(&z[i]);
+    __m128 vz_v = _mm_load_ps(&vz[i]);
+    __m128 dt_v = _mm_set1_ps(dt);
+    __m128 dt_vz_v = _mm_mul_ps(dt_v, vz_v);
+
+    _mm_store_ps(&z[i], _mm_add_ps(z_v, dt_vz_v));
+
+	  // if (z[i] >= 1.0f || z[i] <= -1.0f) vz[i] *= -1.0f;
+        // if ( ((unsigned) (z[i] - (-1.0f)) <= (1.0f - (-1.0f))) == false) vz[i] *= -1.0f;
+        __m128 z_minus_minus_one = _mm_sub_ps(z_v, minus_one);
+        __m128 one_minus_minus_one = _mm_sub_ps(one, minus_one);
+        __m128 mask = _mm_cmple_ps(z_minus_minus_one, one_minus_minus_one);
+
+        __m128 multiply = _mm_mul_ps(vz_v, minus_one);
+        __m128 res = _mm_or_ps(_mm_and_ps(mask, multiply), _mm_andnot_ps(mask, z_minus_minus_one));
+        _mm_store_ps(&vz[i], res);
+
+	}
+
+	for (; i < N; i++) {
+		z[i] += dt * vz[i];
+	  // if (z[i] >= 1.0f || z[i] <= -1.0f) vz[i] *= -1.0f;
+            if ( ((unsigned) (z[i] - (-1.0f)) <= (1.0f - (-1.0f))) == false) vz[i] *= -1.0f;
 	}
 	t1 = wtime();
 	l3 += (t1 - t0);
